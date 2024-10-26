7d88b64e9a64de89d3428b1d04581f3d6a3a1675
dev4anand <iamanand1702@gmail.com>
2024-10-26 11:04:34 +0530

    design changes

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,3 +2,11 @@ window.onload = function() {
     var preloader = document.querySelector('.preloader');
     preloader.style.display = 'none';
   };
+
+  const button = document.querySelector('.videobutton');
+  const video = button.querySelector('video');
+
+  button.addEventListener('click', () => {
+    video.style.display = 'block'; // Show the video
+    video.play(); // Start playing the video
+  });
